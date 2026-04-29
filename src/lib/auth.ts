d2e7d6dd9268d0e43d2114abf0b6c36434b72391
Raj Patel <rajkpatel1728@gmail.com>
2026-04-30 00:22:59 +0530

    feat: auth setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,21 @@
 import { betterAuth } from "better-auth";
+import { createAuthClient } from "better-auth/react"
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db/index"; // your drizzle instance
+import { db } from "@/db/index";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
+
+    emailAndPassword: { 
+        enabled: true,
+    },
+
+    plugins: [tanstackStartCookies()]
 });
+
+export const authClient = createAuthClient({
+    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+})
